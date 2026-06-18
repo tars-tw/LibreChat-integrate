876fc1a7e7ab777b5a6992f691735ad16232e281
Chrisliao0806 <388chris@gmail.com>
2026-06-18 17:28:00 +0800

    feat: Integrate pwc_tars authentication and user management

--- packages/data-schemas/src/schema/user.ts
+++ packages/data-schemas/src/schema/user.ts
@@ -83,6 +83,9 @@ const userSchema: Schema<IUser> = new Schema<IUser>(
     ldapId: {
       type: String,
     },
+    tarsId: {
+      type: String,
+    },
     githubId: {
       type: String,
     },
@@ -213,6 +216,7 @@ const oAuthIdFields = [
   'openidId',
   'samlId',
   'ldapId',
+  'tarsId',
   'githubId',
   'discordId',
   'appleId',
