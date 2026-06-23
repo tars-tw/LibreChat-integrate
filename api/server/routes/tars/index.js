e211069265874451339c2b2108ae55667d2e4b55
Chrisliao0806 <388chris@gmail.com>
2026-06-23 13:20:05 +0800

    feat: add prompts management for TARS

--- api/server/routes/tars/index.js
+++ api/server/routes/tars/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const domains = require('./domains');
 const knowledge = require('./knowledge');
+const prompts = require('./prompts');
 
 const router = express.Router();
 router.use('/', domains);
 router.use('/', knowledge);
+router.use('/', prompts);
 
 module.exports = router;
