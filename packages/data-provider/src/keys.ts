db07e56067636118ec70181a39a14a64c3af490a
Chrisliao0806 <388chris@gmail.com>
2026-07-24 20:42:58 +0800

    feat: implement TARS model profile management and integration, including whitelisting and reordering of models

--- packages/data-provider/src/keys.ts
+++ packages/data-provider/src/keys.ts
@@ -32,6 +32,7 @@ export enum QueryKeys {
   tarsKnowledgeBaseDocuments = 'tarsKnowledgeBaseDocuments',
   tarsDocumentChunks = 'tarsDocumentChunks',
   tarsModelOptions = 'tarsModelOptions',
+  tarsModels = 'tarsModels',
   tarsPrompts = 'tarsPrompts',
   tarsSysConfigs = 'tarsSysConfigs',
   tarsMcpServers = 'tarsMcpServers',
