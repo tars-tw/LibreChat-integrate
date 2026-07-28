0a51dd5ad0b56215faa705f6cfc7bb21b2016c0b
Chrisliao0806 <388chris@gmail.com>
2026-07-28 13:39:03 +0800

    feat: implement specialized brain selection and display in the model selector

--- client/src/components/Chat/Menus/Endpoints/components/EndpointItem.tsx
+++ client/src/components/Chat/Menus/Endpoints/components/EndpointItem.tsx
@@ -10,6 +10,7 @@ import MarketplaceItem, { marketplaceSearchMatches } from './Marketplace';
 import { filterModels, shouldRenderEndpointOption } from '../utils';
 import { useModelSelectorContext } from '../ModelSelectorContext';
 import { renderEndpointModels } from './EndpointModelItem';
+import { DomainItems } from '../../Tars/DomainItems';
 import { ModelSpecItem } from './ModelSpecItem';
 import { useLocalize } from '~/hooks';
 import { cn } from '~/utils';
@@ -135,6 +136,12 @@ function EndpointMenuContent({
 
   return (
     <>
+      {isAgentsEndpoint(endpoint.value) && (
+        <DomainItems
+          searchValue={searchValue}
+          showSeparator={showMarketplace || hasSelectableRows}
+        />
+      )}
       {showMarketplace && <MarketplaceItem label={localize('com_agents_marketplace')} />}
       {showMarketplace && hasSelectableRows && <CustomMenuSeparator />}
       {endpointSpecs.map((spec: TModelSpec) => (
