04e90209a1f08e6d3da438c5cde2dabfcb6754d9
milk7472 <milk7472@gmail.com>
2026-08-04 16:46:50 +0800

    feat: 移除註冊與忘記密碼功能並補上部分多語系

--- client/src/components/Auth/Login.tsx
+++ client/src/components/Auth/Login.tsx
@@ -109,29 +109,16 @@ function Login() {
           setError={setError}
         />
       )}
-      {(startupConfig?.registrationEnabled === true || startupConfig?.tarsAuth === true) && (
-        <p
-          className={`my-4 text-center text-sm font-light ${
-            startupConfig?.tarsAuth === true ? 'text-gray-700' : 'text-text-secondary'
-          }`}
-        >
+      {startupConfig?.tarsAuth !== true && startupConfig?.registrationEnabled === true && (
+        <p className="my-4 text-center text-sm font-light text-text-secondary">
           {' '}
           {localize('com_auth_no_account')}{' '}
-          {startupConfig?.tarsAuth === true ? (
-            <a
-              href={registerPage()}
-              className="inline-flex p-1 text-sm font-medium text-pwc-orange hover:underline"
-            >
-              {localize('com_auth_sign_up')}
-            </a>
-          ) : (
-            <a
-              href={registerPage()}
-              className="inline-flex p-1 text-sm font-medium text-accent-primary underline decoration-transparent transition-all duration-200 hover:text-accent-primary-hover hover:decoration-accent-primary-hover focus:text-accent-primary-hover focus:decoration-accent-primary-hover"
-            >
-              {localize('com_auth_sign_up')}
-            </a>
-          )}
+          <a
+            href={registerPage()}
+            className="inline-flex p-1 text-sm font-medium text-accent-primary underline decoration-transparent transition-all duration-200 hover:text-accent-primary-hover hover:decoration-accent-primary-hover focus:text-accent-primary-hover focus:decoration-accent-primary-hover"
+          >
+            {localize('com_auth_sign_up')}
+          </a>
         </p>
       )}
     </>
