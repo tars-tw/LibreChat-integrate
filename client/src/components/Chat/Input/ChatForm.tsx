ea76f8652a2f7ac9ea5222a37a2cd57702eed89b
milk7472 <milk7472@gmail.com>
2026-08-07 11:37:48 +0800

    feat: 調整免責聲明到聊天框的下方

--- client/src/components/Chat/Input/ChatForm.tsx
+++ client/src/components/Chat/Input/ChatForm.tsx
@@ -55,6 +55,7 @@ import AudioRecorder from './AudioRecorder';
 import AutoPlayAudio from './AutoPlayAudio';
 import CollapseChat from './CollapseChat';
 import QuoteButton from './QuoteButton';
+import Disclaimer from '../Disclaimer';
 import TokenUsage from './TokenUsage';
 import StopButton from './StopButton';
 import SendButton from './SendButton';
@@ -779,6 +780,7 @@ const ChatForm = memo(function ChatForm({
               {TextToSpeech && automaticPlayback && <AutoPlayAudio index={index} />}
             </div>
           </div>
+          <Disclaimer />
         </div>
       </div>
     </form>
