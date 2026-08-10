45b64c6ed6e934bbb9b7f0236698f27e120ce3bc
milk7472 <milk7472@gmail.com>
2026-08-10 12:31:32 +0800

    feat: 更換Librechat顯示字串為TARS.ai

--- api/server/services/AuthService.js
+++ api/server/services/AuthService.js
@@ -227,7 +227,7 @@ const sendVerificationEmail = async (user) => {
     email,
     subject: 'Verify your email',
     payload: {
-      appName: process.env.APP_TITLE || 'LibreChat',
+      appName: process.env.APP_TITLE || 'TARS.ai',
       name: user.name || user.username || email,
       verificationLink: verificationLink,
       year: new Date().getFullYear(),
@@ -493,7 +493,7 @@ const requestPasswordReset = async (req) => {
       email: user.email,
       subject: 'Password Reset Request',
       payload: {
-        appName: process.env.APP_TITLE || 'LibreChat',
+        appName: process.env.APP_TITLE || 'TARS.ai',
         name: user.name || user.username || user.email,
         link: link,
         year: new Date().getFullYear(),
@@ -544,7 +544,7 @@ const resetPassword = async (userId, token, password) => {
       email: user.email,
       subject: 'Password Reset Successfully',
       payload: {
-        appName: process.env.APP_TITLE || 'LibreChat',
+        appName: process.env.APP_TITLE || 'TARS.ai',
         name: user.name || user.username || user.email,
         year: new Date().getFullYear(),
       },
@@ -876,7 +876,7 @@ const resendVerificationEmail = async (req) => {
       email: user.email,
       subject: 'Verify your email',
       payload: {
-        appName: process.env.APP_TITLE || 'LibreChat',
+        appName: process.env.APP_TITLE || 'TARS.ai',
         name: user.name || user.username || user.email,
         verificationLink: verificationLink,
         year: new Date().getFullYear(),
