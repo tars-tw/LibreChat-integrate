c9f7131c74eb844f3963495e7f1a509b8a897a1f
milk7472 <milk7472@gmail.com>
2026-08-20 11:05:32 +0800

    feat: 系統參數設定樣式同步其他管理頁面

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -64,9 +64,9 @@ const loadKnowledgeBaseDetail = () =>
     Component: m.KnowledgeBaseDetail,
   }));
 
-const loadSystemConfigView = () =>
-  import('~/components/SystemConfig').then((m) => ({
-    Component: m.SystemConfigView,
+const loadSysConfigView = () =>
+  import('~/components/Admin/Tars/SysConfig').then((m) => ({
+    Component: m.SysConfigView,
   }));
 
 const loadMcpSettingsView = () =>
@@ -233,7 +233,7 @@ export const router = createBrowserRouter(
             },
             {
               path: 'system-config',
-              lazy: loadSystemConfigView,
+              lazy: loadSysConfigView,
             },
             {
               path: 'mcp-settings',
